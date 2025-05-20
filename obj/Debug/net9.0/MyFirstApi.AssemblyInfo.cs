739cc00f2e8326a24ac9ce56c893866b32ff86b0
EAX-PC <x3010@outlook.com>
2025-05-20 14:46:23 +0300

    Update AssemblyInfo with new informational version and cache hash

--- obj/Debug/net9.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyFirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8091429299cd0840a9c2f847caf2afe81af29fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
